69c8843823047ff7497da88987748f0f4654d5c9
desie2610 <168661973+desie2610@users.noreply.github.com>
2025-03-27 17:18:15 +0200

    lklkl

--- js/number.js
+++ js/number.js
@@ -31,7 +31,32 @@
 // ===================== MATH
 
 
-console.log(Math.min (1,2,3,4,5,6,7,8,9,10) ); // Мінамальне число
-console.log(Math.max (1,2,3,4,5,6,7,8,9,10,) ); // Максимальне число
+// console.log(Math.min (1,2,3,4,5,6,7,8,9,10) ); // Мінамальне число
+// console.log(Math.max (1,2,3,4,5,6,7,8,9,10,) ); // Максимальне число
+
+
+// console.log(Math.pow (8,2) ); // до степеня
+
+//console.log(Math.floor (3.4) ); // округлення до найменшого цілого числа
+//console.log(Math.ceil (8.2) ); // округлення до більшоого цілого числа
+//console.log(Math.round (12.1) );  //округлення за метематичними правилами
+
+//console.log(Math.sqrt (9898) ) // квадратний корінь
+
+//const max = 100;
+//const min = 10;
+
+//console.log(Math.round(Math.random () * (max-min) + min )); // заокруглене рандомне число
+
+// Напишіть скрипт, який знаходить площу прямокутника. Висота 23см, шириною 10см - всі данні повинні зберігатися в змінних.  Значення площі повинно зберігається в числовій змінній.
+
+const heigt = Number.parseInt(prompt("Ведіть висоту"));
+const size = Number.parseInt(prompt("Ведіть ширину"));
+console.log(heigt);
+console.log(size);
+const result = heigt * size;
+console.log(result);
+
+
 
 
