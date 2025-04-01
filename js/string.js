8d40e008458d0ecc49e047d5c6cf44d89116b013
desie2610 <168661973+desie2610@users.noreply.github.com>
2025-04-01 17:49:16 +0300

    qweqwe

---
+++ js/string.js
@@ -0,0 +1,26 @@
+// const a = 12;
+// const b = a.toString(); // перетворення числа в рядок
+// console.log(typeof(b));
+
+// const message = "I love JavaScript"
+// console.log(typeof(message));
+// const firstIndex = message[7];
+// console.log(firstIndex);
+
+
+// console.log(message.length); // вимірює довжину рядка та масива
+
+
+// const lastIndex = message.length -1;
+// console.log(lastIndex);
+
+
+// const lastElement = message[message.length -1]; // отримати останній елемент на рядку або на масиві
+// console.log(lastElement);
+
+
+
+
+
+
+
