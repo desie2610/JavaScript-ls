80451caf6dc7992b4cc01d032b09b8eb3880a8eb
desie2610 <168661973+desie2610@users.noreply.github.com>
2025-04-03 17:37:17 +0300

    uiuui

--- js/string.js
+++ js/string.js
@@ -19,6 +19,56 @@
 // console.log(lastElement);
 
 
+// console.log(message.length);// довжина рядка 
+// console.log(message[2]); // довжина рядка по індексу
+// console.log(message[message.length -3]); // довжина рядка з зворотнього напрямку
+
+
+// console.log(message.toLocaleLowerCase()); // малі літери
+// console.log(message.toLocaleUpperCase()); // великі літери
+
+
+// console.log("потужно" === "ПОТУЖНО");
+
+
+// console.log(message.indexOf("мем")); // шукає індекс першої літери елемента
+// console.log(message.indexOf("шгшщщш")); // якщо не знайдено -1
+// console.log(message.includes("потужно")); // true or false якщо знайде
+// console.log(message.indexOf("мем") !== -1);
+
+// console.log(message.startsWith("амогус мем")); // true or false перевіряє чи починається рядок на ці символи
+// console.log(message.endsWith("мем")); // true or false перевіряє чи закінчується рядок на ці символи
+
+// const picture = "picture.jpeg";
+// console.log(message.endsWith(".jpeg"));
+
+// const userName = prompt("Введіть ім'я").trim(); // прибирає пробіли в початку і в кінці рядка
+// const user = "obama"
+// console.log(userName === user);
+const message = "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Saepe quae commodi ipsam aliquam quia, reprehenderit impedit iure et. Laudantium, fugit omnis obcaecati voluptatibus quos quaerat totam odio velit quas non."; 
+
+console.log(message.slice(0, -2)); // робить зріз починаючи з якого і по який
+
+
+// const content = "974345663";
+// console.log(content.length);
+// const renameContent = content.padStart(13, "+38");
+// console.log(renameContent.length);
+// console.log(content.padStart(16, "(+380) ")); // на початок рядка підставляє якесь значення яке треба додати
+// console.log(content.padEnd(16, "(+380) "));
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
