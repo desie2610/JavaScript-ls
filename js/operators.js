bed5bbf820d268ea560c16d9893dc6459dc24686
desie2610 <168661973+desie2610@users.noreply.github.com>
2025-04-08 17:35:48 +0300

    ls-6

---
+++ js/operators.js
@@ -0,0 +1,24 @@
+// 6 значень які призводять до false
+// Порожній рядочок
+// Сам false
+// none
+// undefined
+// 0
+// null
+// console.log();
+// console.log(false);
+// console.log(none);
+// console.log(undefined);
+// console.log(0);
+// console.log(null);
+
+// решта буде true
+// console.log(Boolean(1));
+
+// Логічний оператор і && якщо всі значеня true повертає останнє значеня якщо зустрічає false на ньому і зупиняється
+// console.log(2 && 3);
+// console.log(null && undefined);
+// console.log(2 && false);
+
+// // логічний операто || якщо одне значення true 
+// console.log(null || "qwerty");
