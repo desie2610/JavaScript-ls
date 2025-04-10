9d6926dd2dace88848f5a5171aad8b2fc0908ff2
desie2610 <168661973+desie2610@users.noreply.github.com>
2025-04-10 17:31:27 +0300

    jjjjj

---
+++ js/rozhaluxhennya.js
@@ -0,0 +1,19 @@
+//  if (умова яка має призвестись до true) {
+//      тіло умови
+// }
+
+// const number = Number(prompt("Введіть число"))
+// if (number > 0) {
+//     console.log(`число ${number} більше 0`);  
+// }
+
+const password = "qwerty"
+const enter = prompt("Введіть пароль")
+if (password === enter) {
+    console.log("ось секретні документи");
+} else {
+    console.log("Ні!!! Ви не отримаєте секретних документів");
+    
+}
+
+    
