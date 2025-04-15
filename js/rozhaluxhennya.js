8e2677335da46db4867fa32295bee26531da96d2
desie2610 <168661973+desie2610@users.noreply.github.com>
2025-04-15 17:49:17 +0300

    yrete

--- js/rozhaluxhennya.js
+++ js/rozhaluxhennya.js
@@ -7,13 +7,49 @@
 //     console.log(`число ${number} більше 0`);  
 // }
 
-const password = "qwerty"
-const enter = prompt("Введіть пароль")
-if (password === enter) {
-    console.log("ось секретні документи");
-} else {
-    console.log("Ні!!! Ви не отримаєте секретних документів");
+// const password = "qwerty"
+// const enter = prompt("Введіть пароль")
+// if (password === enter) {
+//     console.log("ось секретні документи");
+// } else {
+//     console.log("Ні!!! Ви не отримаєте секретних документів");
     
-}
+// }
+
+    // let count = 0; // глобальна облать видимості
+    // if (true) {
+    
+    //     count = 5; // блокова область видимості
+    //     let count
+    //     console.log(count);
+        
+    // }
+
+    // console.log(count);
+
+    let age;
+    // console.log(age);
+    
+    if (true) {
+     age = 20;
+    //  console.log(age);
+     
+     if (true) {
+        age = 40;
+        //  console.log(age);
+         
+        if (true) {
+            age = 100;
+            console.log(age);
+            const name = "Тимофій";
+            // console.log(name);
+            
+        }
+    }
+    }
+    console.log(name);
+    
+  console.log(age);
+  
 
     
