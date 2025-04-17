b230ae76dd5502b24ae3171a8ec190c9b213a184
desie2610 <168661973+desie2610@users.noreply.github.com>
2025-04-17 17:48:47 +0300

    kklllkkkk'k

--- js/rozhaluxhennya.js
+++ js/rozhaluxhennya.js
@@ -25,31 +25,133 @@
         
     // }
 
-    // console.log(count);
+     // console.log(count);
 
-    let age;
-    // console.log(age);
+//     let age;
+//     // console.log(age);
     
-    if (true) {
-     age = 20;
-    //  console.log(age);
+//     if (true) {
+//      age = 20;
+//     //  console.log(age);
      
-     if (true) {
-        age = 40;
-        //  console.log(age);
+//      if (true) {
+//         age = 40;
+//         //  console.log(age);
          
-        if (true) {
-            age = 100;
-            console.log(age);
-            const name = "Тимофій";
-            // console.log(name);
+//         if (true) {
+//             age = 100;
+//             console.log(age);
+//             const name = "Тимофій";
+//             // console.log(name);
             
-        }
-    }
-    }
-    console.log(name);
+//         }
+//     }
+//     }
+//     console.log(name);
     
-  console.log(age);
+//   console.log(age);
   
-
+// const number = Number(prompt("Введіть число від 1 до 5"));
+// if (number === 1) {
+//     console.log("число 1");
+// }
+//  else if (number === 2) {
+//     console.log("число 2");
     
+//  }
+//  else if (number === 3) {
+//     console.log("число 3");
+    
+//  }
+//  else if (number === 4) {
+//     console.log("число 4");
+    
+//  }
+//  else if (number === 5) {
+//     console.log("число 5");
+    
+//  }
+
+//  else if (isNaN(number))
+//  {ви ввели не число}
+
+
+//  else  {
+//     console.log("Ви не ввели число від 1 до 5");
+    
+//  }
+
+// const number = Number(prompt("Введіть число від 1 до 5"));
+
+// switch (number) {
+//     case 1:
+//         console.log('число = 1');
+//         break;
+//     case 2:
+//         console.log('число = 2');
+//         break;
+//     case 3:
+//         console.log("число = 3");
+//         break;
+//     case 4:
+//         console.log("число = 4");
+//         break;
+//     case 5:
+//         console.log("число = 5");
+//             break;
+//     case isNaN(number):
+//         console.log("ви ввели не число");
+//             break;
+//             default:
+//                 console.log("жоден кейс не спрацював");
+                
+        // 1. Напиши скрипт вибору опції доставки товару. Опція зберігається в змінній option: 1 - самовивіз, 2 - кур"єр, 3 - пошта. В змінну message записати повідомлення в залежності від опції:
+
+        
+
+
+
+        
+        // switch (option) {
+        //     case 1:
+        //         message = "самовивіз";
+        //         break;
+
+        //     case 2:
+        //         message = "кур'єр"
+        //         break;
+
+        //     case 3 :
+        //         message = "пошта";
+        //         break;       
+                
+        //         default:
+        //             message = "Ви ввели число не від 1 до 3";
+                
+        // }
+        //     let message = "";
+
+        //     const option = Number(prompt("виберть опцію доставки від 1 до 3"))
+
+        //     if (option === 1) {
+        //         message = "самовивіз";
+        //     }
+
+        //     else if (option === 2) {
+        //         message = "кур'єр";
+        //     }
+
+        //     else if (option === 3) {
+        //         message = "пошта";
+        //     }
+
+        //     else {
+        //         message = "Ви ввели не коректне число"
+        //     }
+            
+        // console.log(message);
+
+        
+        
+        
+
