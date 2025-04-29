8dc820bdd7b0c250fb1462b46723ead0902e012f
desie2610 <168661973+desie2610@users.noreply.github.com>
2025-04-29 21:10:16 +0300

    fghfghfgh

--- js/aray.js
+++ js/aray.js
@@ -16,3 +16,48 @@
 //     }
 // }
 // console.log(sum);
+
+// const aray = [['a', 'b', 'c'], ['d', 'e', 'f']]
+// console.log(aray[0]);
+// console.log(aray[1][0]);
+
+// for (let i = 0; i < aray.length; i += 1) {
+//     console.log(i);
+    
+// }
+
+
+// const friends = ["artem", "anton", "david", "oleksander", "timofii", "yaroslav", "daria", "katya"]
+// const findName = 'oleksander';
+
+// for (let i = 0; i < friends.length; i += 1) {
+//     console.log(friends[i]);
+//        if (friends[i] === findName) {
+//        console.log(`Вашого друга ${friends[i]} знайдено під індексом ${i}`);
+//                 break
+//      }
+//  }
+
+// }
+
+
+// const number = ["0951234567", "0683223195", "050459566", "0723222454"]
+//     for (let i = 0; i < number.length; i +=1) {
+//         console.log(number[i].length);
+        
+        
+
+//          if (number[i].length < 10) {
+//             console.log(`Номер телефону ${number[i]} не дійсний!`);
+            
+//          }
+//     }
+
+
+
+const number = ["0951234567", "0683223195", "0504595669", "0723222454"]
+for(let i = 0; i < number.length; i += 1) {
+    console.log("+38" + number[i] );
+    
+}
+  
