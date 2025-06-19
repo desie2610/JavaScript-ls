1d0fdf9323f9033b13e26fe0f38fd032a0ea7f6d
desie2610 <168661973+desie2610@users.noreply.github.com>
2025-06-19 17:40:07 +0300

    asdasd

---
+++ js/metodi-misivu.js
@@ -0,0 +1,41 @@
+// перебираючий метот map він повертає такий же масив (кількість) як і оригінальний й засовує в новий
+
+// const numbers = [1,2,3,4,5,6,7,8,9]
+// const result = numbers.map((item) => item*2)
+
+// console.log(result);
+
+// const people = [
+//     {age: 15, name: 'тимофій'},
+//     {age: 14, name: 'тимофій'},
+//     {age: 13, name: 'тимофій'},
+//     {age: 12, name: 'тимофій'},
+// ]
+
+// const res = people.map((item) => item.age + 10)
+// console.log(res);
+
+// const result = people.map((item) => item.name)
+// console.log(result);
+
+// перебираючий метот фільтр перебираэ масив до кожного елементу масиву застосовує колл бек функцію повертає новий масив в який додає елементи які призводяться до true
+
+const people = [
+    {age: 15, name: 'тимофій', isactive: true},
+    {age: 14, name: 'тимофій', isactive: true},
+    {age: 13, name: 'тимофій', isactive: false},
+    {age: 12, name: 'тимофій', isactive: false},
+]
+
+// const res = people.filter((item) => item.age > 12).map((item) => item.name)
+// console.log(res);
+
+// const filtresactive people.filter((a) => a.isactive).map((b) => b.city)
+
+
+
+
+
+
+
+
